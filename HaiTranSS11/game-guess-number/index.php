d53c66d690a5600d82d579ae4f333c7b1444d34e
Hai.Tran <tranhaicoder@gmail.com>
2019-12-16 20:59:45 +0700

    update file n-th

--- HaiTranSS11/game-guess-number/index.php
+++ HaiTranSS11/game-guess-number/index.php
@@ -3,6 +3,9 @@ session_start();
 //session_destroy();
 define("DEFAULT_LOW", 1);
 define("DEFAULT_HIGH", 100);
+define("TRUE_NUMBER", "true");
+define("FALSE_LOWER", "false-lower");
+define("FALSE_HIGHER", "false-higher");
 
 $reload = false;
 
@@ -13,24 +16,30 @@ if (!isset($_SESSION["low"])) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if ($_SESSION["low"] >= $_SESSION["high"]) {
-        echo "You're kidding me? Get out the here";
+    if ($_SESSION["low"] >= $_SESSION["high"] || $_SESSION["low"] < 1 || $_SESSION["high"] > 100) {
+        echo "You're kidding me? I code this game not to you do it! Stupid fool!<br>";
+        echo "Just press F5 to play again! If you're enough smart!";
         $reload = true;
         unset($_POST["isRight"]);
         session_destroy();
-    }
-    if ($_POST["isRight"] == "true") {
-        echo "<br>";
-        echo "Yeah! I did it. Please click on URL and enter if you want to try again!";
-        $reload = true;
-        unset($_POST["isRight"]);
-        session_destroy();
-    }
-    elseif ($_POST["isRight"] == "false-lower") {
-        $_SESSION["high"] = --$_SESSION["guessNumber"];
-    } else {
-        $_SESSION["low"] = ++$_SESSION["guessNumber"];
-    }
+    } else
+        switch ($_POST["isRight"]) {
+            case TRUE_NUMBER:
+                echo "Yeah! I did it. Reload or press F5 to play again!";
+                $reload = true;
+                unset($_POST["isRight"]);
+                session_destroy();
+                break;
+            case FALSE_LOWER:
+                $_SESSION["high"] = --$_SESSION["guessNumber"];
+                break;
+            case FALSE_HIGHER:
+                $_SESSION["low"] = ++$_SESSION["guessNumber"];
+                break;
+            default:
+                if (!empty($_SESSION["guessNumber"]))
+                    echo "You must choose an answer! Please try again!";
+        }
 }
 
 function guessNumber($low, $high)
@@ -39,8 +48,6 @@ function guessNumber($low, $high)
 }
 
 $_SESSION["guessNumber"] = guessNumber($_SESSION["low"], $_SESSION["high"]);
-
-//var_dump($_SESSION["low"], $_SESSION["high"], $_SESSION["guessNumber"]);
 
 ?>
 
@@ -53,18 +60,28 @@ $_SESSION["guessNumber"] = guessNumber($_SESSION["low"], $_SESSION["high"]);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        div {
+            position: absolute;
+            margin-left: 30%;
+            padding: 10px;
+            border: 1px solid grey;
+        }
+
+    </style>
 </head>
 <body>
-<div>
-    <?php if (!$reload): ?>
-    <p>Is <?php echo $_SESSION["guessNumber"]; ?>?</p>
-    <form action="" method="post">
-        <label><input type="radio" name="isRight" value="true">Yes, This is my number.</label>
-        <label><input type="radio" name="isRight" value="false-lower">No, My number lower than that.</label>
-        <label><input type="radio" name="isRight" value="false-higher">No, My number higher than that.</label>
-        <input type="submit" value="Submit">
-    </form>
-    <?php endif; ?>
-</div>
+<?php if (!$reload): ?>
+    <div>
+        <h1>Guess Game</h1>
+        <p>Your number is <?php echo $_SESSION["guessNumber"]; ?> ?</p>
+        <form action="" method="post">
+            <label><input type="radio" name="isRight" value="true">Yes, This is my number.</label><br>
+            <label><input type="radio" name="isRight" value="false-lower">No, My number lower than that.</label><br>
+            <label><input type="radio" name="isRight" value="false-higher">No, My number higher than that.</label><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+<?php endif; ?>
 </body>
 </html>
